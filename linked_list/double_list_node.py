10b50e1a47a4113b553ce3abe4f9d5008e3bb787
Chris Dotson <csdotson1@gmail.com>
2018-06-28 10:23:56 -0700

    Add class for doubly-linked node

---
+++ linked_list/double_list_node.py
@@ -0,0 +1,7 @@
+class DoubleListNode:
+    """ Doubly linked node """
+    def __init__(self, data=0, prev=None, next=None):
+        self.data = data
+        self.prev = prev
+        self.next = next
+        
